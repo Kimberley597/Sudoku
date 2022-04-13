c7b693ad807ce18942b1649cf963eae91163444e
Kimberley Goodman <40457190@live.napier.ac.uk>
2022-04-13 13:09:55 +0100

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sudoku
+{
+    class Program
+    {
+        // store varibales that are part of the game
+        // like undo, redo, game board - globaly availiable to entire game loop 
+        static void Main(string[] args)
+        {
+            // first ask what difficulty the user wants
+            // generate the sudoku board - pass in difficulty parameter, return game baord (design a class to handle game board)
+        }
+    }
+}
